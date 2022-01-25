81d1aab83ffc07eecf38cbc8b0a4ec50d0c6e40b
Y0ussy <youszey@gmail.com>
2022-01-25 14:37:15 +0100

    Popup color

--- name-popup/script.js
+++ name-popup/script.js
@@ -1,5 +1,6 @@
 
-var color = prompt("give me a color?");
-var body = document.getElementsByClassName("BODY")[0];
-body.style.backgroundcolor = color;
+var color = prompt("give me a color?"); // dit stukje code, gaat een popup tonen (specifiek een vraag stellen)
+var el = document.getElementsByTagName("body")[0]; // daarin gaan we het HTML-element steken met de naam body
+console.log(el);
+el.style.backgroundColor = color;
 
