7edf9dcfc50ab09c83d0b027def70b68c1e490c4
Y0ussy <youszey@gmail.com>
2022-02-01 16:40:10 +0100

    GDFGFG

---
+++ Wie-ben-ik/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('Youssef oefening werkt');
+
+
+let h1Element = document.querySelector('.h1'); 
+
+console.log(h1Element);
+
+h1Element.style.backgroundColor = 'grey';
+
+
+let Youssef = document.querySelector('#Youssef');
+
+Youssef.style.textDecoration= underline;
+
+
+
+
+
+
+
+
