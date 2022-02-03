75a7979fabe8b96335393d843456fa983d164ba6
Y0ussy <youszey@gmail.com>
2022-02-03 16:37:03 +0100

    gfgfgf

--- Wie-ben-ik/script.js
+++ Wie-ben-ik/script.js
@@ -3,16 +3,40 @@
 console.log('Youssef oefening werkt');
 
 
-let h1Element = document.querySelector('.h1'); 
+
+let h1Element = document.querySelector('.h1');
 
 console.log(h1Element);
 
 h1Element.style.backgroundColor = 'grey';
 
 
+
 let Youssef = document.querySelector('#Youssef');
 
-Youssef.style.textDecoration= underline;
+Youssef.style.textDecoration = "underline";
+
+
+
+let klasseNaam = 'derde-rij';
+
+let derderij = document.getElementsByClassName(klasseNaam);
+
+for (let element of derderij) {
+    console.log(element);
+    element.style.backgroundColor = 'grey';
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
