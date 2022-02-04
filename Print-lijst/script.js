73bd32607fe16b40274a3fd41a30cf942d8935c7
Y0ussy <youszey@gmail.com>
2022-02-04 11:50:35 +0100

    New update

---
+++ Print-lijst/script.js
@@ -0,0 +1,13 @@
+
+var Ronaldo = {Genre: "Sport" , Afkomst: "Portugees" , Hobby:"Voetballen" };
+
+var Dicaprio = {Genre: "Acteur" , Afkomst: "Amerikaans" , Hobby: "Acteren" };
+
+var Pewdiepie = {Genre: "Entertainer" , Afkomst: "Zweeds" , Hobby: "filmpjes maken" };
+
+var lijstartiesten = [Ronaldo, Dicaprio, Pewdiepie];
+
+for (let artiesten of lijstartiesten) {
+    console.log(artiesten);
+}
+
