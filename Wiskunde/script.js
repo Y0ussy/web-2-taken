e9796904ab797410c14a6934d9dda53e6defec60
Y0ussy <youszey@gmail.com>
2022-02-08 16:11:51 +0100

    update 4

--- Wiskunde/script.js
+++ Wiskunde/script.js
@@ -6,9 +6,29 @@ let getal1 = prompt('Geef het eerste getal');
 
 let getal2 = prompt('Geef de tweede getal');
 
-let som =(getal*1) + (getal*2); // de som van de getallen hierboven.
+let som = (getal1*1) + (getal2*1); // de som van de getallen hierboven.
                                            
-console.log(getal,)
-
 alert(som);
+
+
+if (getal1 > getal2) {
+    alert('getal 1 is kleiner')
+} else  {
+    alert('getal 2 is groter')
+}
+
+
+
+// we emoeten getal 1 en 2 vergelijken
+let isGetal1KleinerDanGetal2 = getal1 < getal2; 
+
+// bij if controleren we als iets juist of onjuist is
+if (isGetal1KleinerDanGetal2) { 
+    //dit voeren we uit indien juist
+    alert('Getal 1 is kleiner dan getal 2');
+} else if (isGetal1KleinerDanGetal2) { 
+    // dit voeren we uit indien fout
+    alert('Getal 1 is groter dan getal 2');
+}
+
 
