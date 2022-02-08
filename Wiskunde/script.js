9d7862ff87b0720f856e42f623645919c4ab2921
Y0ussy <youszey@gmail.com>
2022-02-08 14:09:57 +0100

    WISKUNDE UPDATE

---
+++ Wiskunde/script.js
@@ -0,0 +1,14 @@
+
+// Dit geeft een alert bericht met de oplossing van de bewerking.
+
+
+let getal1 = prompt('Geef het eerste getal');
+
+let getal2 = prompt('Geef de tweede getal');
+
+let som =(getal*1) + (getal*2); // de som van de getallen hierboven.
+                                           
+console.log(getal,)
+
+alert(som);
+
