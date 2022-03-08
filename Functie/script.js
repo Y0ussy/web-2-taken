21da117c698314e3325f019a55ff229d1f16da50
Y0ussy <youszey@gmail.com>
2022-03-08 16:16:45 +0100

    Update last

--- Functie/script.js
+++ Functie/script.js
@@ -8,9 +8,15 @@ let lijstArtiesten = [Ronaldo, Dicaprio, Pewdiepie];
 console.log(lijstArtiesten.length);
 
 for (let i = 0; i < lijstArtiesten.length; i++) {
-    console.log(lijstArtiesten[i]);
+  
+    artiestPrinten(lijstArtiesten[i]);
 
 }
+
+function artiestPrinten(parameter) {
+    console.log(parameter.Genre + ' ' + parameter.Afkomst + ' ' + parameter.Hobby)
+}
+
 
 
 
