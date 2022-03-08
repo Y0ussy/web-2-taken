8115dc4eb7f09d41b0e8a20af6734937c007432a
Y0ussy <youszey@gmail.com>
2022-03-08 15:03:37 +0100

    update

---
+++ Functie/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let Ronaldo = {Genre: "Sport" , Afkomst: "Portugees" , Hobby:"Voetballen" };
+let Dicaprio = {Genre: "Acteur" , Afkomst: "Amerikaans" , Hobby: "Acteren" };
+let Pewdiepie = {Genre: "Entertainer" , Afkomst: "Zweeds" , Hobby: "filmpjes maken" };
+let lijstArtiesten = [Ronaldo, Dicaprio, Pewdiepie];
+
+console.log(lijstArtiesten.length);
+
+for (let i = 0; i < lijstArtiesten.length; i++) {
+    console.log(lijstArtiesten[i]);
+
+}
+
+
+
